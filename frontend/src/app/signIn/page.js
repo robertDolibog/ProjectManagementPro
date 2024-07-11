8316fcbe5d48b5ed4873ac1c2bd171d88720ca73
Robert <robert.dolibog@outlook.de>
2024-07-11 22:46:19 +0200

    added getting Started Page and auto forwarding after signin/up

--- frontend/src/app/signIn/page.js
+++ frontend/src/app/signIn/page.js
@@ -37,12 +37,36 @@ export default function LoginForm() {
       const data = await response.json();
 
       console.log("data response in frontend after signin", data);
+      console.log("response in frontend after signin", response);
 
-      if (response.ok) {
+      if (response.ok ) {
         // Sign-in or sign-up was successful
-        // Redirect to the user's projects page
+        // Fetch all pages for the user
+        const pagesResponse = await fetch("http://localhost:4000/pages", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-        router.push("/");
+        const pagesData = await pagesResponse.json();
+
+        if (pagesResponse.ok) {
+          // Find the most recent page
+          const mostRecentPage = pagesData.reduce((latestPage, currentPage) => {
+            const latestDate = new Date(latestPage.createdAt);
+            const currentDate = new Date(currentPage.createdAt);
+            return currentDate > latestDate ? currentPage : latestPage;
+          });
+
+          // Redirect to the most recent page
+          router.push(`/pages/${mostRecentPage.id}`);
+        } else {
+          // Handle error in fetching the pages
+          console.log("Error: Unable to fetch the pages", pagesData);
+          setError("Unable to fetch the pages.");
+        }
       } else {
         // Sign-in or sign-up failed
         // Show an error message
@@ -53,14 +77,15 @@ export default function LoginForm() {
       console.error("Error making fetch request:", error);
     }
   };
+
   return (
-    <main className=" h-screen flex flex-col justify-center items-center bg-black">
+    <main className="h-screen flex flex-col justify-center items-center bg-black">
       <form
         onSubmit={handleSubmit}
         suppressHydrationWarning={true}
-        className="  w-auto flex flex-col items-center mt-12"
+        className="w-auto flex flex-col items-center mt-12"
       >
-        <section className="bg-green-900 z-20  flex flex-col w-[35vw] items-center px-20 py-10 rounded-lg">
+        <section className="bg-green-900 z-20 flex flex-col w-[35vw] items-center px-20 py-10 rounded-lg">
           <h1 className="text-white text-2xl font-bold">
             {isLogin ? "Login to see your Projects" : "Register for an account"}
           </h1>
@@ -115,3 +140,4 @@ export default function LoginForm() {
     </main>
   );
 }
+
